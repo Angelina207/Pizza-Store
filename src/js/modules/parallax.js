b3e3044c15e4d8bb31901347db74cb17072eb516
Angelina Kuzmenko <kuzmenkoa022@gmail.com>
2022-08-05 22:28:18 +0300

    general

--- src/js/modules/parallax.js
+++ src/js/modules/parallax.js
@@ -1,16 +1,28 @@
 export function parallaxEffect() {
 
     function parallax (e) {
-
         let offsetX = e.clientX;
         let offsetY = e.clientY;
 
         this.querySelectorAll('.scene__layer').forEach(layer => {
             let speedAttribute = layer.getAttribute('data-speed');
 
-            console.log(layer.style.transform = `translateX(${offsetX / window.innerWidth * speedAttribute}px) translateY(${offsetY / window.innerHeight * speedAttribute}px)`)
+            layer.style.transform = `translateX(${offsetX / window.innerWidth * speedAttribute}px) translateY(${offsetY / window.innerHeight * speedAttribute}px)`;
         })
     }
 
+    function windowScroll (e) {
+        let offsetScroll = window.scrollY;
+        let scene = document.querySelector('.scene');
+
+            if (offsetScroll > 1000) {
+                scene.style.display = 'none';
+                
+            } else {
+                scene.style.display = 'block';
+        }
+    }
+
     document.addEventListener('mousemove', parallax);
+    document.addEventListener('scroll', windowScroll);
 }
